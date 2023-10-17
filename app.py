9a77652125e3cf8f6e4713a780fbbea1652fb536
Gayatri <gayatri29mehta@gmail.com>
2023-10-17 22:34:21 +0530

    hyperlinked registration and vaccination schedule page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,25 +8,17 @@ child_data = []
 
 @app.route('/')
 def home():
-    return "Welcome to the Child Immunization Service"
+    return "welcome to child immunization service"
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        # Process the submitted form data
-        child_name = request.form.get('child_name')
-        age = request.form.get('age')
-        # Add the child's data to the temporary database
-        child_data.append({'child_name': child_name, 'age': age})
-        # Implement scheduling logic here based on age
-        # For demonstration, just redirect to the scheduling page
-        return redirect(url_for('schedule'))
+
     return render_template('registration.html')
 
 @app.route('/schedule')
 def schedule():
     # Display the child's schedule based on the data in child_data
-    return render_template('vaccination_schedule.html', child_data=child_data)
+    return render_template('vaccination_schedule.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
